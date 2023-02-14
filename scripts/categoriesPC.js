7e3f09986dad1e53f0f1e4cc764c011f11924f7c
João Vitor <jovifsc@gmail.com>
2023-02-13 22:20:45 -0300

    feat(add) new feature 'dropdown' in categories.

---
+++ scripts/categoriesPC.js
@@ -0,0 +1,27 @@
+let button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+  let filter = document.querySelector(".filter");
+  filter.classList.toggle("active");
+});
+
+let button2 = document.getElementById("button2");
+
+button2.addEventListener("click", function () {
+  let categories = document.querySelector(".game-categories ");
+  categories.classList.toggle("active");
+});
+
+let button3 = document.getElementById("button3");
+
+button3.addEventListener("click", function () {
+  let industries = document.querySelector(".game-industries ");
+  industries.classList.toggle("active");
+});
+
+let button4 = document.getElementById("button4");
+
+button4.addEventListener("click", function () {
+  let range = document.querySelector(".price-range ");
+  range.classList.toggle("active");
+});
