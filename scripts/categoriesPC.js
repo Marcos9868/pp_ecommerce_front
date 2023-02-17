629c57ee32e91e2345ee409c13a8f6afda76106b
João Vitor <jovifsc@gmail.com>
2023-02-17 20:54:36 -0300

    fix: modals(filters) in pc.html

--- scripts/categoriesPC.js
+++ scripts/categoriesPC.js
@@ -8,20 +8,14 @@ button.addEventListener("click", function () {
 let button2 = document.getElementById("button2");
 
 button2.addEventListener("click", function () {
-  let categories = document.querySelector(".game-categories ");
+  let categories = document.querySelector(".game-categories");
   categories.classList.toggle("active");
 });
 
 let button3 = document.getElementById("button3");
 
 button3.addEventListener("click", function () {
-  let industries = document.querySelector(".game-industries ");
+  let industries = document.querySelector(".game-industries");
   industries.classList.toggle("active");
 });
 
-let button4 = document.getElementById("button4");
-
-button4.addEventListener("click", function () {
-  let range = document.querySelector(".price-range ");
-  range.classList.toggle("active");
-});
